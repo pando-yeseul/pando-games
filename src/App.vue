d565617f76368fafd860b09df73996135fbafc10
yeah2114 <yeah2114@naver.com>
2021-01-20 17:09:30 +0900

    210120_v2_yeseul_add

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="main">
+      <router-view />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import Main from './components/Main'
+
+export default {
+  name: 'App',
+  components: {
+    Header,Footer,}
+}
+</script>
+
+<style>
+
+</style>
